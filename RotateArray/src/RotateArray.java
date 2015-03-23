d954535a9818833715fd33103b35959061d6b1a9
CuiXun <gcuixun@gmail.com>
2015-03-23 00:24:59 -0400

    rotate in O(n)

---
+++ RotateArray/src/RotateArray.java
@@ -0,0 +1,32 @@
+/**
+ * Created by cuixun on 3/23/15.
+ */
+public class RotateArray {
+    public static void main(String[]args){
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        rotate(array,5);
+    }
+
+    /**
+     * Time complexity O(n);
+     * @param nums
+     * @param k
+     */
+    public static void rotate(int[] nums, int k) {
+        int length = nums.length;
+        int[]b = new int[length];
+        for(int i = 0; i < length; i++){
+            b[i] = nums[i];
+        }
+        for(int i = 0, j = length - k; j < length; i++, j++){
+            nums[i] = b[j];
+        }
+        for(int i = k, j = 0; i < length; i++, j++){
+            nums[i] = b[j];
+        }
+
+        for(int i = 0; i < nums.length; i++){
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
