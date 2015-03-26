71fb95cddea4f6327f2b8e1552c9b81cc84531c4
CuiXun <gcuixun@gmail.com>
2015-03-25 22:16:35 -0400

    with hashmap, time complexity is O(n) and space complexity is O(n)

--- TwoSum/src/TwoSum.java
+++ TwoSum/src/TwoSum.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by cuixun on 3/24/15.
  */
@@ -5,11 +8,25 @@ public class TwoSum {
     public static void main(String[]args){
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 13, 23, 34, 45, 56, 67, 78, 89};
         int[] out = targetSumIndex(array, 167);
+        ArrayList<Integer> list = twoSumIndex(array, 167);
+        
         for(int i = 0; i < out.length; i++){
+
             System.out.print(out[i]+ " ");
+        }
+
+        System.out.println();
+        for(int i = 0; i < list.size(); i++){
+            System.out.print(list.get(i)+ " ");
         }
     }
 
+    /**
+     * We suppose the array has been sorted
+     * @param array
+     * @param targetSum
+     * @return
+     */
     public static int[] targetSumIndex(int[] array, int targetSum){
         int[] result = {-1, -1};
         for(int i = 0; i < array.length; i++){
@@ -17,8 +34,8 @@ public class TwoSum {
             int target = targetSum - data1;
             int j = isSearched(target, array, i + 1, array.length - 1);
             if(j > 0){
-                result[0] = i;
-                result[1] = j;
+                result[0] = i + 1;
+                result[1] = j + 1;
                 return result;
             }
         }
@@ -33,13 +50,37 @@ public class TwoSum {
             if (mid == target) {
                 return index;
             }
-            int x = isSearched(target, array, left, index - 1);
-            int y = isSearched(target, array, index + 1, right);
-            if (x > 0) {
-                return x;
+            if(target < mid) {
+                int x = isSearched(target, array, left, index - 1);
+                if (x > 0) {
+                    return x;
+                }
             }
+            int y = isSearched(target, array, index + 1, right);
             return y;
         }
         return -1;
+    }
+
+    /**
+     * we return ArrayList here to separate the previous method
+     * the time complexity here is O(n) space complexity here is O(n)
+     * @param array
+     * @param target
+     * @return
+     */
+    public static ArrayList<Integer> twoSumIndex(int[] array, int target){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0; i < array.length; i++){
+            int x = target - array[i];
+            if(map.containsKey(target - x)){
+                list.add(map.get(target - x) + 1);
+                list.add(i + 1);
+                return list;
+            }
+            map.put(x, i);
+        }
+        return list;
     }
 }
